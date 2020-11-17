08b2df533cd7f2f01b2751cbf027f0bfe0e5cf8c
SanheHu <4038571439@GSA.GOV>
2020-11-16 23:36:42 -0500

    buildDockerImage library

---
+++ README.rst
@@ -0,0 +1,2 @@
+ISE Jenkins Shared Library
+======
---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,30 @@
+/*
+@param artifactoryRegistry example 'art-010-gp-ociso-image'
+@param pushImageName The image name we will Push to our repository
+@param pushImageTag The image tag we will apply to the image we Push to our repository
+@param dockerFile The Docker file we will use to build the image we will Push to our repository
+
+
+    // String artifactoryRegistry,
+    // String pushImageName,
+    // String pushImageTag,
+    // String dockerFile,
+    
+*/
+def call(
+) {
+    /*
+    Build the Docker image using the Docker file name and tag as defined in the associated variables.
+    
+    Note: We force remove any intermediate containers and do not use cache.
+        The reason for not using cache is to force Dockerfile steps that install the latest packages from an
+        external repository to always run even if that step has not changes from the perspective of Docker.
+        ie. If you change a packages minor version upstream in the Pipeline but your install steps for this package
+        do not specify minor version the cached image will see this as an already met requirement and not reinstall
+        if caching is enabled.
+    */
+    script {
+        // docker.build("${artifactoryRegistry}/${pushImageName}:${pushImageTag}", "-f ${dockerFile} --no-cache --pull ./")
+        echo "Build Image"
+    }
+}
