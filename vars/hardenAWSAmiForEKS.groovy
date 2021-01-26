10fc27da3c9e48604211cfc56408578e152962a5
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-01-26 18:28:33 -0500

    Update hardenAWSAmiForEKS.groovy

--- vars/hardenAWSAmiForEKS.groovy
+++ vars/hardenAWSAmiForEKS.groovy
@@ -19,7 +19,7 @@ def call () {
             // reference: https://www.jenkins.io/doc/book/pipeline/syntax/
             docker {
                 // image 'sanhe/cicd:aws-ultimate@sha256:7cdb672755b8e743e9f35d2719b0111f9750c92682baddaf2670f5ad065d7164'
-                image 'hashicorp/packer'
+                image 'jenkins/jnlp-agent-python'
                 registryUrl 'https://registry.hub.docker.com'
                 registryCredentialsId 'rjlupinek-dockerhub'
                 args '-e JENKINS_HOME=$JENKINS_HOME -e AWS_DEFAULT_REGION="us-east-1"'
@@ -29,7 +29,17 @@ def call () {
             stage('verify dependencies') {
                 steps {
                     script {
-                        sh "which packer"
+                        sh """
+                        echo "install wget"
+                        sudo apk add wget -y
+                        which wget
+                        echo "install packer"
+                        wget "https://releases.hashicorp.com/packer/${PACKER_VERSION}/packer_${PACKER_VERSION}_linux_amd64.zip" -P "${HOME}" -q
+                        unzip "${HOME}/packer_${PACKER_VERSION}_linux_amd64.zip" -d "${HOME}"
+                        mv "${HOME}/packer" "/usr/local/bin"
+                        rm "${HOME}/packer_${PACKER_VERSION}_linux_amd64.zip"
+                        """
+                        // sh "which packer"
                     }
                 }
             }
