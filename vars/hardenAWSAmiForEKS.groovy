d16372a364a91927c76843f05ec996cc67adcb32
Sanhe <MacHu-GWU@users.noreply.github.com>
2021-01-25 19:14:12 -0500

    Update hardenAWSAmiForEKS.groovy

--- vars/hardenAWSAmiForEKS.groovy
+++ vars/hardenAWSAmiForEKS.groovy
@@ -8,9 +8,8 @@ def call () {
         agent {
             // use a container with aws-cli, ansible, packer, python pre-installed
             // reference: https://www.jenkins.io/doc/book/pipeline/syntax/
-            docker { 
-                image 'hashicorp/packer'
-                // image 'sanhe/cicd:aws-ultimate@sha256:7cdb672755b8e743e9f35d2719b0111f9750c92682baddaf2670f5ad065d7164'
+            docker {
+                image 'sanhe/cicd:aws-ultimate@sha256:7cdb672755b8e743e9f35d2719b0111f9750c92682baddaf2670f5ad065d7164'
                 registryUrl 'https://registry.hub.docker.com'
                 registryCredentialsId 'rjlupinek-dockerhub'
                 args '-e JENKINS_HOME=$JENKINS_HOME -e AWS_DEFAULT_REGION="us-east-1"'
@@ -20,25 +19,22 @@ def call () {
             stage('verify dependencies') {
                 steps {
                     script {
-                        // sh "which ansible"
-                        sh "which packer"
                         sh "which aws"
+                        sh "which packer"
                         sh "which python"
-                        sh "which pip"
-                        sh "pip list"
                     }
                 }
             }
-            // stage('run packer build') {
-            //     steps {
-            //         script {
-            //             sh "pwd"
-            //             sh "aws sts get-caller-identity" // verify the build runtime has sufficent previledge in its IAM Role
-            //             sh "echo JENKINS_HOME=${JENKINS_HOME}"
-            //             sh "bash ./packer/run-packer-build.sh"
-            //         }
-            //     }
-            // }
+            stage('run packer build') {
+                steps {
+                    script {
+                        sh "pwd"
+                        sh "aws sts get-caller-identity" // verify the build runtime has sufficent previledge in its IAM Role
+                        sh "echo JENKINS_HOME=${JENKINS_HOME}"
+                        sh "bash ./packer/run-packer-build.sh"
+                    }
+                }
+            }
         }
     }
 }
